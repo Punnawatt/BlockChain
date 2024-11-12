05210653b46dc05c62643a69b4ad09fb3b8083ca
Punnawatt <punnawathonglerdnapakul@gmail.com>
2024-11-12 19:56:52 +0700

    fix: bracket

--- Frontend/src/Mymain.jsx
+++ Frontend/src/Mymain.jsx
@@ -1,47 +1,45 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 //import MyApp from './MyApp.jsx';
 
-import HomePage from './module/HomePage/page.jsx';
-import CreateProblem from './module/CreateProblem/page.jsx';
-import SubmitCode from './module/SubmitCode/page.jsx';
-import Grader from './module/Grader/page.jsx';
-import './index.css';
+import HomePage from "./module/HomePage/page.jsx";
+import CreateProblem from "./module/CreateProblem/page.jsx";
+import SubmitCode from "./module/SubmitCode/page.jsx";
+import Grader from "./module/Grader/page.jsx";
+import "./index.css";
 //import ProblemDetail from './module/ProblemDetail/page.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>,
+    element: <HomePage />,
   },
   {
     path: "/HomePage",
-    element: <HomePage/>,
+    element: <HomePage />,
   },
   {
     path: "/CreateProblem",
-    element: <CreateProblem/>,
+    element: <CreateProblem />,
   },
   {
     path: "/SubmitCode",
-    element: <SubmitCode/>,
+    element: <SubmitCode />,
   },
   {
     path: "grader",
-    element: <Grader />
-  }
-  }
+    element: <Grader />,
+  },
   // {
   //   path: "/problem/:id",
   //   element: <ProblemDetail/>,
   // },
-  
-  
+
   // Add more routes here as needed
 ]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
